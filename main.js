a3d7a8fc38b31d98d62063c1918badbfc59f8332
Abby Howell <abby.g.howell@gmail.com>
2013-11-01 16:35:28 -0400

    starting work

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 // Application namespace
+var RCApp = {
+
+	var Label: function() {
+    this.artists = [];
+	},
+
+	var Artist: function(name, description) {
+    this.name = name;
+    this.description = description;
+    this.albums = [];
+	},
+
+	var Album: funtion(name, description, genre) {
+    this.name = name;
+    this.description = description;
+    this.genre = genre;
+    this.artists = [];
+	},
+
+};
