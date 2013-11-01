3ae2a106a06d005ee6cd0e894e07acb2c14e5cf9
Abby Howell <abby.g.howell@gmail.com>
2013-11-01 17:46:27 -0400

    moving functions outside of namespace curly brackets

--- main.js
+++ main.js
@@ -7,59 +7,67 @@ var RCApp = {
   new_album_button: document.getElementById('add-album'),
   new_label_button: document.getElementById('add-label'),
 
-  albums: [],
-  artists: [],
-  labels: [],
-
-
+  albums: document.getElementById('albums'),
+  artists: document.getElementById('artists'),
+  labels: document.getElementById('labels'),
+  
   setButtonEvents: function() {
     RCApp.new_artist_button.addEventListener('click', RCApp.new_artist_button_response);
     RCApp.new_album_button.addEventListener('click', RCApp.new_album_button_response);
     RCApp.new_label_button.addEventListener('click', RCApp.new_label_button_response);
   },
 
-  Album: function(name, description, genre) {
-    this.name = name;
-    this.description = description;
-    this.genre = genre;
-    this.artists = [];
-	},
-
-	new_album_button_response: function() {
-		var name = prompt('Album Name');
-		var description = prompt('Album Description');
-		var genre = prompt('Album Genre');
-		RCApp.albums.push(new RCApp.Album(name, description, genre));
-	},
-
-	Label: function(name) {
-		this.name = name;
-    this.artists = [];
-	},
-
-	new_label_button_response: function() {
-		var name = prompt('Record Label Name');
-	  RCApp.labels.push(new RCApp.Label(name));
-	},
-
-	Artist: function(name, description) {
-    this.name = name;
-    this.description = description;
-    this.albums = [];
-  },
-
-  RCApp.Artist.prototype.generateHTML: function() {
-
-  }
-
-  new_artist_button_response: function() {
-		var name = prompt('Artist Name');
-		var description = prompt('Artist Description');
-		RCApp.artists.push(new RCApp.Artist(name, description));
-	},
 };
 
-
-
-
 RCApp.setButtonEvents();
+
+RCApp.MusicThing = function() {
+};
+
+RCApp.MusicThing.prototype.generateHTML = function() {
+      return '<li>' + this.name + '</li>';
+};
+
+RCApp.Album = new RCApp.MusicThing();
+RCApp.Album = function(name, description, genre) {
+  this.name = name;
+  this.description = description;
+  this.genre = genre;
+  this.artists = [];
+  this.prototype = RCApp.MusicThing.prototype;
+};
+
+RCApp.new_album_button_response = function() {
+	var name = prompt('Album Name');
+	var description = prompt('Album Description');
+	var genre = prompt('Album Genre');
+	var new_album = new RCApp.Album(name, description, genre);
+	RCApp.albums.innerHTML += new_album.generateHTML;
+};
+
+RCApp.Artist = new RCApp.MusicThing();
+RCApp.Artist = function(name, description) {
+  this.name = name;
+  this.description = description;
+  this.albums = [];
+};
+
+RCApp.new_artist_button_response = function() {
+	var name = prompt('Artist Name');
+	var description = prompt('Artist Description');
+	var new_artist = new RCApp.Artist(name, description);
+  RCApp.artists.innerHTML += new_artist.generateHTML();
+};
+
+RCApp.Label = new RCApp.MusicThing();
+RCApp.Label = function(name) {
+	this.name = name;
+  this.artists = [];
+};
+
+RCApp.new_label_button_response = function() {
+	var name = prompt('Record Label Name');
+	var new_label = new RCApp.Label(name);
+  RCApp.labels.innerHTML += new_label.generateHTML();
+};
+
