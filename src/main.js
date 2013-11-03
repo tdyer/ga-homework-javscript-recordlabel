50547a71d067e03529321405278322440255c427
Ben Winter <ben.g.winter@gmail.com>
2013-11-03 15:06:24 -0500

    working album and artists

--- src/main.js
+++ src/main.js
@@ -1,71 +1,116 @@
 var RCApp = {
+  
+  addLabelButton: document.getElementById('add-label-button'),
+  addArtistButton: document.getElementById('add-artist-button'),
+  createArtistButton: document.getElementById('create-artist-button'),
+  addAlbumButton: document.getElementById('add-album-button'),
+  createAlbumButton: document.getElementById('create-album-button'),
+  labels: [],
 
-  addLabelButtonEvent: function() {
-    document.getElementById('add-label-button').addEventListener('click', function(event) {
-      var labelform = document.getElementById('add-label')
-      var name = labelform.elements[0].value;
+  addEventListeners: function() {
+    RCApp.addLabelButton.addEventListener('click', function(event) {
       event.preventDefault();
-      new RCApp.recordLabel(name);
-      document.getElementById('label-name').innerHTML = name;
-      labelform.style.display = 'none';
-      RCApp.addArtistButtonEvent();
+      RCApp.addLabelButtonResponse();
+    });
+
+    RCApp.addArtistButton.addEventListener('click', function(event) {
+      event.preventDefault();
+      RCApp.addArtistButtonResponse();
+    });
+
+    RCApp.createArtistButton.addEventListener('click', function(event) {
+      event.preventDefault();
+      RCApp.createArtistButtonResponse();
+    });
+
+    RCApp.addAlbumButton.addEventListener('click', function(event) {
+      event.preventDefault();
+      RCApp.addAlbumButtonResponse();
+    });
+
+    RCApp.createAlbumButton.addEventListener('click', function(event) {
+      event.preventDefault();
+      RCApp.createAlbumButtonResponse();
     });
   },
 
-  addArtistButtonEvent: function() {
-    var addArtistButton = document.getElementById('add-artist-button');
-    addArtistButton.style.display = 'block';
-    addArtistButton.addEventListener('click', function(event) {
-      event.preventDefault();
-      addArtistButton.style.display = 'none';
-      RCApp.createArtistButtonEvent();
-    });
-
+  addLabelButtonResponse: function() {
+    var labelform = document.getElementById('add-label')
+    var name = labelform.elements[0].value;
+    new RCApp.RecordLabel(name);
+    labelform.style.display = 'none';
+    RCApp.addArtistButton.style.display = 'block';
+    RCApp.addAlbumButton.style.display = 'block';
   },
 
-  createArtistButtonEvent: function() {
+  addArtistButtonResponse: function() {
     var artistForm = document.getElementById('add-artist-form');
-    var createArtistButton = document.getElementById('create-artist-button');
     artistForm.style.display = 'block';
-    createArtistButton.addEventListener('click', function(event) {
-        event.preventDefault();
-        new RCApp.addArtist(artistForm.elements[0].value, artistForm.elements[1].value);
-        artistForm.style.display = 'none';
-        RCApp.addArtistButtonEvent();
-      });
-    RCApp.removeEventListeners();
+    RCApp.addArtistButton.style.display = 'none';
+  },
+
+  createArtistButtonResponse: function() {
+    var artistForm = document.getElementById('add-artist-form');
+    new RCApp.Artist(artistForm.elements[0].value, artistForm.elements[1].value);
+    artistForm.style.display = 'none';
+    RCApp.addArtistButton.style.display = 'block';
+  },
+
+  addAlbumButtonResponse: function() {
+    var albumForm = document.getElementById('add-album-form');
+    albumForm.style.display = 'block';
+    RCApp.addAlbumButton.style.display = 'none';
+  },
+
+  createAlbumButtonResponse: function() {
+    var albumForm = document.getElementById('add-album-form');
+    new RCApp.Album(albumForm.elements[0].value, albumForm.elements[1].value, albumForm.elements[2].value);
+    albumForm.style.display = 'none';
+    RCApp.addAlbumButton.style.display = 'block';
   },
 
   listArtists: function() {
     var i = 0, list = document.getElementById('artists-list'), artistsArray = RCApp.labels[0].artists; 
     list.innerHTML = "";
     for (; i < artistsArray.length;) {
-        list.innerHTML += "<li> <h4> Artist: " + artistsArray[i].name + "</h4> </li>";
-        i += 1;
-      };
+      list.innerHTML += "<li> <h4> Artist: " + artistsArray[i].name + "</h4> </li>";
+      i += 1;
+    };
   },
 
-  labels: [],
+  listAlbums: function() {
+    var i = 0, list = document.getElementById('albums-list'), albumsArray = RCApp.labels[0].albums; 
+    list.innerHTML = "";
+    for (; i < albumsArray.length;) {
+      list.innerHTML += "<li> <h4> Album: " + albumsArray[i].title + "</h4> </li>";
+      i += 1;
+    };
+  },
 
-  recordLabel: function(name) {
+  RecordLabel: function(name) {
     this.name = name;
     this.artists = [];
     this.albums = [];
     RCApp.labels.push(this);
+    document.getElementById('label-name').innerHTML = this.name;
   },
 
-  addArtist: function(name, description) {
+  Artist: function(name, description) {
     this.name = name;
     this.description = description;
     RCApp.labels[0].artists.push(this);
     RCApp.listArtists();
   },
 
-  album: function(title) {
+  Album: function(title, description, genre) {
     this.title = title;
-    artists: [];
+    this.description = description;
+    this.genre = genre;
+    this.artists = [];
+    RCApp.labels[0].albums.push(this);
+    RCApp.listAlbums();
   }
 
 };
 
-RCApp.addLabelButtonEvent();
+RCApp.addEventListeners();
