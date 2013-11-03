65f2dfd653ccc5c88a2a6cf20a37624179a809b6
Abby Howell <abby.g.howell@gmail.com>
2013-11-03 16:56:59 -0500

    artists and albums displaying descriptions.  i haven't figured out drop down boxes yet

--- main.js
+++ main.js
@@ -19,7 +19,8 @@ var RCApp = {
   artists: document.getElementById('artists'),
   labels: document.getElementById('labels'),
 
-  all_artists: []
+  all_artists: [],
+  all_albums: []
 
 };
 
@@ -50,12 +51,17 @@ RCApp.Album = function(name, description, genre) {
   this.name = name;
   this.description = description;
   this.genre = genre;
-  // this.artists = [];
 };
 
 RCApp.Album.prototype.generateHTML = function() {
   var html = '<li>' + this.name + '</li>';
   document.getElementById('albums').innerHTML += html;
+};
+
+RCApp.Album.prototype.generateHTML = function() {
+  var new_li = document.createElement('li');
+  new_li.innerHTML = '<a href=\"add_album\" id=\"' + this.name + '_link\"cllink-item\" >' + this.name + '</a>';
+  document.getElementById('albums').appendChild(new_li);
 };
 
 RCApp.show_album_form_button_response = function() {
@@ -64,13 +70,31 @@ RCApp.show_album_form_button_response = function() {
 };
 
 RCApp.create_new_album = function() {
-	var name = document.getElementById('album-name-input').value;
-	var description = document.getElementById('album-description-input').value;
-	var genre = document.getElementById('album-genre-input').value;
-	var new_album = new RCApp.Album(name, description, genre);
+	var name = document.getElementById('album-name-input').value,
+	description = document.getElementById('album-description-input').value,
+	genre = document.getElementById('album-genre-input').value,
+	new_album = new RCApp.Album(name, description, genre);
+	RCApp.all_albums.push(new_album);
 	new_album.generateHTML();
+	new_album.set_link_event();
 	RCApp.new_album_form.className = 'hide';
 	RCApp.show_album_form_button.className = 'show';
+};
+
+
+RCApp.Album.prototype.set_link_event = function() {
+  var link = document.getElementById(this.name + '_link');
+  var object = this;
+  link.addEventListener('click', function(event) {
+    event.preventDefault();
+    RCApp.highlight_name(link, object);
+  });
+};
+
+
+RCApp.show_album_form_button_response = function() {
+	RCApp.show_album_form_button.className = 'hide';
+	RCApp.new_album_form.className = 'show';
 };
 
 ///////////////////////
@@ -94,44 +118,71 @@ RCApp.create_new_artist = function() {
 
 RCApp.Artist.prototype.generateHTML = function() {
   var new_li = document.createElement('li');
-  new_li.innerHTML = '<a href=\"add_artist\" id=\"' + this.name + '_link\"class=\"artist-link-item\" >' + this.name + '</a>';
+  new_li.innerHTML = '<a href=\"add_artist\" id=\"' + this.name + '_link\"class=\"link-item\" >' + this.name + '</a>';
+  // var new_button = document.createElement('button');
+  // new_button.innerHTML = 'Add Album to Artist\'s Catalog';
   document.getElementById('artists').appendChild(new_li);
+  // new_li.appendChild(new_button);
+  // new_button.addEventListener('click', RCApp.generateDropDownMenu);
 };
 
+// RCApp.generateDropDownMenu = function() {
+// 	var select = document.createElement('select'),
+// 	i = 0,
+// 	max = RCApp.all_albums.length;
+// 	for (; i < max ;) {
+// 		var option = document.createElement('option');
+// 		option.value = RCApp.all_albums[i];
+// 		option.textContent = RCApp.all_albums[i].name;
+// 	  select.appendChild(option);
+// 	  i += 1;
+//   }
+//   this.parentNode.appendChild(select);
+// };
+
+// RCApp.Artist.prototype.addAlbum = function() {
+  
+// };
+
 RCApp.Artist.prototype.set_link_event = function() {
-  var artist_link = document.getElementById(this.name + '_link');
-  var artist_object = this;
-  artist_link.addEventListener('click', function(event) {
+  var link = document.getElementById(this.name + '_link');
+  var object = this;
+  link.addEventListener('click', function(event) {
     event.preventDefault();
-    RCApp.highlight_artist_name(artist_link, artist_object);
+    RCApp.highlight_name(link, object);
   });
 };
 
-RCApp.highlight_artist_name = function(artist_link, artist_object) {
-  var all_links = document.getElementsByClassName('artist-link-item'),
+RCApp.highlight_name = function(link, object) {
+  var all_links = document.getElementsByClassName('link-item'),
   i = 0,
   max = all_links.length;
   for (; i < max; ) {
     all_links[i].className += ' no-highlight';
     i += 1;
   }
-  artist_link.className = 'artist-link-item';
-  artist_link.className += ' highlight';
-  RCApp.display_artist_info(artist_object);
+  link.className = 'link-item';
+  link.className += ' highlight';
+  RCApp.display_info(object);
 };
 
-RCApp.display_artist_info = function(artist) {
-var description_div = document.getElementById('desc');
-description_div.innerHTML = '<p>' + artist.description + '</p>';
-	// if (artist.albums) {
-	//   var i = 0, max = artist.albums.length, album_list = '<ul></ul>';
-	//   for (; i < max ;) {
-	//     album_list.innerHTML += '<li>' + artist.albums[i].name + '</li>';
-	//     i += 1;
-	//   }
-	//   description_div.innerHTML += album_list;
-	// }
+
+RCApp.display_info = function(object) {
+	var description_div = document.getElementById('desc');
+	var object_description = '<p>' + object.description + '</p>';
+	description_div.innerHTML = object_description;
+	if (object.albums) {
+	  var i = 0, max = object.albums.length, album_list = document.createElement('ul');
+	  for (; i < max ;) {
+	    var list_item =  '<li>' + object.albums[i].name + '</li>';
+	    album_list.appendChild(list_item);
+	    i += 1;
+	  }
+	description_div.innerHTML.appendChild(album_list);
+  }
+
 };
+
 
 RCApp.show_artist_form_button_response = function() {
 	RCApp.show_artist_form_button.className = 'hide';
