40382af8919b05c743768b6114bfef7bbddf409d
Ben Winter <ben.g.winter@gmail.com>
2013-11-03 14:15:13 -0500

    merged with bryces

--- src/main.js
+++ src/main.js
@@ -1,52 +1,71 @@
-// namespace for our application
-var EventLab = EventLab || {};
+var RCApp = {
 
-EventLab.button1 = document.getElementById('button1');
-// All the different types of events
-// https://developer.mozilla.org/en-US/docs/Web/Reference/Events#Standard_events
-// http://www.w3.org/TR/DOM-Level-3-Events/#event-interfaces
+  addLabelButtonEvent: function() {
+    document.getElementById('add-label-button').addEventListener('click', function(event) {
+      var labelform = document.getElementById('add-label')
+      var name = labelform.elements[0].value;
+      event.preventDefault();
+      new RCApp.recordLabel(name);
+      document.getElementById('label-name').innerHTML = name;
+      labelform.style.display = 'none';
+      RCApp.addArtistButtonEvent();
+    });
+  },
 
-// 0) Show the inline event handler in the index.html
+  addArtistButtonEvent: function() {
+    var addArtistButton = document.getElementById('add-artist-button');
+    addArtistButton.style.display = 'block';
+    addArtistButton.addEventListener('click', function(event) {
+      event.preventDefault();
+      addArtistButton.style.display = 'none';
+      RCApp.createArtistButtonEvent();
+    });
 
-// 1) Simple event handler
-//EventLab.button1.addEventListener('click', function(){
-// 	alert('You clicked the button');
-// });
+  },
 
-// 2) Abstract out the 2 ways to catch/handle an event
-// register button1 click handler.
-// registerEventHandler(button1, "click", showEvent);
+  createArtistButtonEvent: function() {
+    var artistForm = document.getElementById('add-artist-form');
+    var createArtistButton = document.getElementById('create-artist-button');
+    artistForm.style.display = 'block';
+    createArtistButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        new RCApp.addArtist(artistForm.elements[0].value, artistForm.elements[1].value);
+        artistForm.style.display = 'none';
+        RCApp.addArtistButtonEvent();
+      });
+    RCApp.removeEventListeners();
+  },
 
-// 3) Capture other events sent when clicking on the button.	
-// Note the order of the events sent/generated.
-// registerEventHandler(button1, "dblclick", showEvent);
-// registerEventHandler(button1, "mousedown", showEvent);
-// registerEventHandler(button1, "mouseup", showEvent);
+  listArtists: function() {
+    var i = 0, list = document.getElementById('artists-list'), artistsArray = RCApp.labels[0].artists; 
+    list.innerHTML = "";
+    for (; i < artistsArray.length;) {
+        list.innerHTML += "<li> <h4> Artist: " + artistsArray[i].name + "</h4> </li>";
+        i += 1;
+      };
+  },
 
-// 4) Show how events are 'bubbled up' to parent document elements.
-// register a handler on the whole document.
-// http://www.w3.org/TR/DOM-Level-3-Events/#event-flow
-// Every click event on any element in the document will be 
-// "bubbled up" to the document
- EventLab.body = document.getElementsByTagName('body')[0];
-// registerEventHandler(EventLab.body, "click", reportClick);
+  labels: [],
 
-// 4.1) Stop the event from bubbling up to parent elements in the 
-// DOM
- // registerEventHandler(EventLab.button1, "click", showEvent);
- // registerEventHandler(EventLab.button1, "click", stopEvent);
- // registerEventHandler(EventLab.body, "click", reportClick);
+  recordLabel: function(name) {
+    this.name = name;
+    this.artists = [];
+    this.albums = [];
+    RCApp.labels.push(this);
+  },
 
-// 5) Generate an event in the chrome inspector/debugger.
-// document.getElementById('button1').click();
+  addArtist: function(name, description) {
+    this.name = name;
+    this.description = description;
+    RCApp.labels[0].artists.push(this);
+    RCApp.listArtists();
+  },
 
-// 6) Unregister event.
-// unregisterEventHandler(document, "click", reportClick);
+  album: function(title) {
+    this.title = title;
+    artists: [];
+  }
 
-// 7) Register a handler for the text box keydown/up and keypress events.
+};
 
-EventLab.text1 = document.getElementById('text1');
-//registerEventHandler(EventLab.text1, "keydown", printKeyCode);
-// unregisterEventHandler(EventLab.text1, "keydown", printKeyCode);
-
-// registerEventHandler(EventLab.text1, "keypress", printCharacter);
+RCApp.addLabelButtonEvent();
