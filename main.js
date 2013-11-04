e0c394cd30e8122b5b7fa649352670f64411d742
Dan Crews <dancrews@me.com>
2013-11-03 21:31:44 -0500

    added methods to add album and add artist

--- main.js
+++ main.js
@@ -1,21 +1,43 @@
 'use strict';
 
 // Application namespace
-var RCApp = RCApp|| {};
+var RCApp = {
+	artists: [],
+	albums: []
 
-
-function RecordLabel(name) {
+RCApp.RecordLabel = function() {
+	// hidden form
+	//add artist button - name, description
 	this.name = name;
-	this.artists = [];
-  this.albums = [];
-}
+};
 
-function Artist(name) {
+RCApp.Artist = function() {
+	var name;
+	var description;
+
 	this.name = name;
+	this.description = description;
 	this.albums = [];
-}
+};
 
-function Album(name) {
+RCApp.Album = function() {
+	var name;
+
 	this.name = name;
 	this.artists = [];
+},
+
+RCApp.add_album = function(dest) {
+	var new_album = new RCApp.Album();
+  dest.albums.push(new_album);
+},
+
+RCApp.add_artist = function(dest) {
+	var new_artist = new RCApp.Artist();
+  dest.artists.push(new_artist);
 }
+
+
+
+
+};
