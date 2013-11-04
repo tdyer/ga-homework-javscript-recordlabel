db3daf0ebdb4992de9ca3a87329ccf4ce8eb4f7c
Dan Crews <dancrews@me.com>
2013-11-04 08:57:44 -0500

    Added requirements and id's to main.js. Started adding album form to index.

--- main.js
+++ main.js
@@ -4,16 +4,38 @@
 var RCApp = {
 	artists: [],
 	albums: []
-
+//	display list of artists
 RCApp.RecordLabel = function() {
 	// hidden form
 	//add artist button - name, description
 	this.name = name;
 };
 
-RCApp.Artist = function() {
-	var name;
-	var description;
+// id='rc-name>'
+// id='rc-description'
+// id='artists'
+// id='desc'
+
+addAlbumButton: document.getElementById('add-album'),
+  // addRecordCompanyButton: document.getElementById('record_company'),
+  // addArtistButton: document.getElementById('add-artist'),
+
+  // artistForm: document.getElementById('artist-form'),
+  albumForm: document.getElementById('album-form'),
+  albumFormSubmitButton: document.getElementById('album-submit-button'),
+  // artistFormSubmitButton: document.getElementById('artist-submit-button')
+
+  // albums: document.getElementById('albums'),
+  // artists: document.getElementById('artists'),
+
+
+
+  RCApp.Artist = function() {
+  	var name;
+  	var description;
+
+	// hidden drop down - albums
+	// add artist to album button
 
 	this.name = name;
 	this.description = description;
@@ -22,20 +44,24 @@ RCApp.Artist = function() {
 
 RCApp.Album = function() {
 	var name;
+	var genre;
+	var description;
 
 	this.name = name;
+	this.genre = genre;
+	this.description = description;
 	this.artists = [];
-},
+};
 
 RCApp.add_album = function(dest) {
 	var new_album = new RCApp.Album();
-  dest.albums.push(new_album);
-},
+	dest.albums.push(new_album);
+};
 
 RCApp.add_artist = function(dest) {
 	var new_artist = new RCApp.Artist();
-  dest.artists.push(new_artist);
-}
+	dest.artists.push(new_artist);
+};
 
 
 
