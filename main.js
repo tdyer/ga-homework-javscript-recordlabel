b7cdacbefbfdd716f3b0874a23c28ea921f35999
Dan Crews <dancrews@me.com>
2013-11-03 20:09:36 -0500

    Added namespace and constructors to main.js

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+'use strict';
+
 // Application namespace
+var RCApp = RCApp|| {};
+
+
+function RecordLabel(name) {
+	this.name = name;
+	this.artists = [];
+  this.albums = [];
+}
+
+function Artist(name) {
+	this.name = name;
+	this.albums = [];
+}
+
+function Album(name) {
+	this.name = name;
+	this.artists = [];
+}
